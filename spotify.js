e1f40d808ac9b34c9aa9d805fa8e52373242433f
Amita  chorghe <67407962+amitachorghe@users.noreply.github.com>
2021-12-13 23:32:11 +0530

    Add files via upload

---
+++ spotify.js
@@ -0,0 +1,52 @@
+let songindex=0;
+let playbutton=document.getElementById("playbutton");
+let progressbar=document.getElementById("progressbar");
+let audioElement=new Audio('./1.mp3')
+let songslist=[
+    {songname:"Agar tum sath ho", path:"./1.mp3"},
+    {songname:"Chahu mai ya na", path:"./audio_2.mp3"},
+    {songname:"Tujhe kitna chah ne lage", path:"./audio_3.mp3"},
+    {songname:"Tose naina", path:"./audio_4.mp3"},
+]
+playbutton.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement<=0){
+        audioElement.play();
+        playbutton.src="./media/pause_circle_outline_white_24dp.svg";
+        document.getElementById("playinggif").style.opacity="1";
+        document.getElementById("songname").style.opacity="1";
+    }
+    else{
+        audioElement.pause();
+        playbutton.src="./media/play-circle-regular.svg"
+        document.getElementById("playinggif").style.opacity="0";
+        document.getElementById("songname").style.opacity="0";
+    }
+})
+audioElement.addEventListener('timeupdate',()=>{
+    let progress=parseInt((audioElement.currentTime/audioElement.duration)*100)
+    progressbar.value=progress;
+})
+progressbar.addEventListener('change',()=>{
+    audioElement.currentTime=progressbar.value*audioElement.duration/100;
+})
+function playall(){
+    Array.from(document.getElementsByClassName('icons')).forEach(e =>{
+        e.src="./media/play-circle-regular.svg"
+    })
+}
+Array.from(document.getElementsByClassName('icons')).forEach(element => 
+    {
+        element.addEventListener('click',()=>{
+            index =parseInt(element.id)
+            console.log(element)
+            playall();
+            element.src="./media/pause_circle_outline_white_24dp.svg";
+            audioElement.currentTime=0;
+            audioElement.src='audio/'+index+'.mp3';
+            console.log(audioElement.src)
+            audioElement.play();
+            playbutton.src="./media/pause_circle_outline_white_24dp.svg";
+            document.getElementById("playinggif").style.opacity="1";
+            document.getElementById("songname").style.opacity="1";
+        })
+});
